83f2957b3feb314ca0b52f25909d7abf1dd4b030
simononboard <sweetsixteen.sudo@gmail.com>
2021-04-24 18:51:46 +0300

    - data for data mining analyzing

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> integerSet = new HashSet<>(List.of(544,32,25,3425));
+        System.out.println(integerSet);
+    }
+}
